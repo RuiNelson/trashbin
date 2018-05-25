6c7167971d135a6ee9ae0f4044489bc52331efe6
Rui Carneiro <rui.nelson@hotmail.com>
2018-05-25 02:20:06 +0100

    de-generalized glob

--- trashbin/Utils.swift
+++ trashbin/Utils.swift
@@ -16,7 +16,7 @@ func printError(_ message: String) {
 func promptYesOrNo(question: String) -> Bool {
     print("\(programName): \(question)")
     var reply: String? = nil
-
+    
     while reply == nil {
         reply = readLine()
         if let rLower = reply?.lowercased() {
@@ -29,31 +29,34 @@ func promptYesOrNo(question: String) -> Bool {
             }
         }
     }
-
+    
     fatalError()
 }
 
 func glob(pattern: String) -> [String] {
+    let globFlags: Int32 = GLOB_MARK | GLOB_NOESCAPE | GLOB_BRACE | GLOB_NOMAGIC | GLOB_TILDE
+    return glob(pattern: pattern, globFlags: globFlags)
+}
+
+func glob(pattern: String, globFlags: Int32) -> [String] {
     func doGlob(pattern: UnsafePointer<Int8>, flags: Int32, globType: UnsafeMutablePointer<glob_t>) -> Int32 {
         return glob(pattern, flags, nil, globType)
     }
-
+    
     var globType = glob_t()
     let pattern = strdup(pattern)!
-    let globFlags: Int32 = GLOB_MARK | GLOB_NOESCAPE | GLOB_BRACE | GLOB_NOMAGIC | GLOB_TILDE
 
     var result: [String] = []
-
+    
     if doGlob(pattern: pattern, flags: globFlags, globType: &globType) == 0 {
         let numberOfMatches = Int(globType.gl_matchc)
-
+        
         for matchNum in 0..<numberOfMatches {
             let match = String(cString: globType.gl_pathv[matchNum]!)
             result.append(match)
         }
-
     }
-
+    
     return result
 }
 
@@ -62,7 +65,7 @@ func fileInfoPrint(path: String, size: Int64?) {
         print(path)
         return
     }
-
+    
     let humanReadable = bcf.string(fromByteCount: size)
     print(path + " (" + humanReadable + ")")
 }
