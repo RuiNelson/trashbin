11ee1240613c6920f32862fa12b3651ba7df1fca
Rui Carneiro <rui.nelson@hotmail.com>
2019-12-11 08:37:57 +0000

    Lint

--- trashbin/ListAndEmpty.swift
+++ trashbin/ListAndEmpty.swift
@@ -4,45 +4,45 @@ import Foundation
 
 func trashBinContents() -> [String] {
 	var contents: [String] = []
-	
+
 	contents.append(contentsOf: glob(pattern: "~/.Trash/*"))
 	contents.append(contentsOf: glob(pattern: "~/.Trash/.*"))
-	
+
 	contents.append(contentsOf: glob(pattern: "/Volumes/*/.Trashes/*/*"))
 	contents.append(contentsOf: glob(pattern: "/Volumes/*/.Trashes/*/.*"))
-	
+
 	contents.append(contentsOf: glob(pattern: "~/Library/Mobile Documents/com~apple~CloudDocs/.Trash/*"))
 	contents.append(contentsOf: glob(pattern: "~/Library/Mobile Documents/com~apple~CloudDocs/.Trash/.*"))
-	
+
 	contents = contents.filter({ (entry) -> Bool in
 		return ( entry.hasSuffix("/../") || entry.hasSuffix("/./") ) == false
-		
+
 	})
-	
+
 	return contents
 }
 
 func listTrashBin() -> Int64? {
 	var total: Int64 = 0
 	let contents = trashBinContents()
-	
+
 	if contents.isEmpty {
 		print("Trash is empty")
 		return nil
 	}
-	
+
 	print("Trash content:")
 	for content in contents {
 		var size: Int64?
-		
+
 		if options.showSize {
 			size = Constants.fileManager.sizeOfItem(atPath: content)
 			total += size ?? 0
 		}
-		
+
 		fileInfoPrint(path: content, size: size)
 	}
-	
+
 	return options.showSize ? total : nil
 }
 
@@ -50,21 +50,21 @@ func emptyTrash() -> Int64? {
 	options.unlink = true
 	options.directories = true
 	options.recursive = true
-	
+
 	var total: Int64 = 0
 	let contents = trashBinContents()
-	
+
 	if contents.isEmpty {
 		printWarning("Trash is already empty")
 		return nil
 	}
-	
-	print ("Emptying the trash...")
+
+	print("Emptying the trash...")
 	for content in contents {
 		let url = URL(fileURLWithPath: content)
-		
+
 		total += execute(url)
 	}
-	
+
 	return options.showSize ? total : nil
 }
